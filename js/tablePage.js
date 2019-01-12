5ff9027002c7819e5f570881da11fba8bc9aac2c
Amit Parjapati2 <amitp2@chetu.com>
2019-01-12 20:57:14 +0530

    Updated for DBA Json

--- js/tablePage.js
+++ js/tablePage.js
@@ -1,62 +1,62 @@
 
-function formValidate(){
-    var name = document.MyForm.name.value;
-    if( name == "" ){
-        alert( "Please provide Name!" );
-        document.myForm.name.focus() ;
-        return false;
-    }
+// function formValidate(){
+//     var name = document.MyForm.name.value;
+//     if( name == "" ){
+//         alert( "Please provide Name!" );
+//         document.myForm.name.focus() ;
+//         return false;
+//     }
 
-    if( name.length < 3 ||  !isNaN( name )){
-        alert( "Please provide a valid name!" );
-        document.myForm.name.focus() ;
-        return false;
-    } 
+//     if( name.length < 3 ||  !isNaN( name )){
+//         alert( "Please provide a valid name!" );
+//         document.myForm.name.focus() ;
+//         return false;
+//     } 
 
-    var design = document.MyForm.design.value;
-    if( design == "" ){
-        alert( "Please provide Designation!" );
-        document.myForm.design.focus() ;
-        return false;
-    }
-    if( design.length < 3 ||  !isNaN( design )){
-        alert( "Please provide a Designation!" );
-        document.myForm.design.focus() ;
-        return false;
-    }
+//     var design = document.MyForm.design.value;
+//     if( design == "" ){
+//         alert( "Please provide Designation!" );
+//         document.myForm.design.focus() ;
+//         return false;
+//     }
+//     if( design.length < 3 ||  !isNaN( design )){
+//         alert( "Please provide a Designation!" );
+//         document.myForm.design.focus() ;
+//         return false;
+//     }
 
-    var expert = document.MyForm.expert.value;
-    if( expert == "" ){
-        alert( "Please provide Exprience!" );
-        document.myForm.expert.focus() ;
-        return false;
-    }
-    if( expert.length < 3 ||  !isNaN( expert )){
-        alert( "Please provide a Valid Exprience!" );
-        document.myForm.expert.focus() ;
-        return false;
-    } 
+//     var expert = document.MyForm.expert.value;
+//     if( expert == "" ){
+//         alert( "Please provide Exprience!" );
+//         document.myForm.expert.focus() ;
+//         return false;
+//     }
+//     if( expert.length < 3 ||  !isNaN( expert )){
+//         alert( "Please provide a Valid Exprience!" );
+//         document.myForm.expert.focus() ;
+//         return false;
+//     } 
      
-    var tech = document.MyForm.tech.value;
-    if( tech == "" ){
-        alert( "Please provide Technology!" );
-        document.myForm.tech.focus() ;
-        return false;
-    }
-    if( tech.length < 3 ||  !isNaN( tech )){
-        alert( "Please provide a Valid Technology!" );
-        document.myForm.tech.focus() ;
-        return false;
-    }
+//     var tech = document.MyForm.tech.value;
+//     if( tech == "" ){
+//         alert( "Please provide Technology!" );
+//         document.myForm.tech.focus() ;
+//         return false;
+//     }
+//     if( tech.length < 3 ||  !isNaN( tech )){
+//         alert( "Please provide a Valid Technology!" );
+//         document.myForm.tech.focus() ;
+//         return false;
+//     }
 
 
-    if( document.MyForm.rate.value == "-1" ){
-        alert( "Please provide Rating!" );
-        return false;
-    }
+//     if( document.MyForm.rate.value == "-1" ){
+//         alert( "Please provide Rating!" );
+//         return false;
+//     }
 
-    return( true ); 
-}
+//     return( true ); 
+// }
 
 
 //Open add Record Modal
@@ -110,85 +110,125 @@ function formValidate(){
       for (j = 0; j < MDismis.length; j++ ){
         MDismis[j].addEventListener("click", hideModal); 
       }
+})();
+
+$(function(){
+  var $addTodoForm = $('#addTodo');
+  var $tbody = $('tbody');
+
+  var url = 'http://localhost:3000/todos';
+
+  //Template
+  var source = $('#listItemTemplate').html();
+  var template = Handlebars.compile(source)
+
+  //Add TodoList
+  $addTodoForm.on('submit', function(e){
+    e.preventDefault();
+    //alert('test');
+    var newTodo = $addTodoForm.find('input').val();
+
+    
+    $addTodoForm.find('input').val('');
+
+    $.ajax({
+      url:url,
+      method:'POST',
+      data: {
+        text: newTodo
+      }
+    }).done(function(newTodo){
+      var listItem = template({
+            text: newTodo.text,
+            id:newTodo.id
+          });
+
+      $tbody.append(listItem)
+    }).fail(function(){
+      //Err
+    })
+  });
 })
 
 
+
+
 //Delete a row from table
-(function(){
-    var i, j, k, l, m;
-    var openModalBtn = document.querySelectorAll('.openModal');
-    var deleteRow = document.querySelectorAll('.deleteRow')
-    var activeM = document.getElementById('deleteRowModal');
+// (function(){
+//     var i, j, k, l, m;
+//     var openModalBtn = document.querySelectorAll('.openModal');
+//     var deleteRow = document.querySelectorAll('.deleteRow')
+//     var activeM = document.getElementById('deleteRowModal');
 
     
-    var tRow = 'null';
+//     var tRow = 'null';
 
-    //Open Modal 
-    function openModal(e){
-        e.preventDefault();
+//     //Open Modal 
+//     function openModal(e){
+//         e.preventDefault();
 
-        //Show Modal
-        activeM.classList.remove('hide');
-        document.body.style.overflow = 'hidden';
+//         //Show Modal
+//         activeM.classList.remove('hide');
+//         document.body.style.overflow = 'hidden';
 
-        // //Add dark shadow behind modal
-        var mShadow = document.createElement('div');
-        mShadow.setAttribute('class','mShadow');    
-        activeM.parentNode.insertBefore(mShadow, activeM);   
-        mShadow.appendChild(activeM);
+//         // //Add dark shadow behind modal
+//         var mShadow = document.createElement('div');
+//         mShadow.setAttribute('class','mShadow');    
+//         activeM.parentNode.insertBefore(mShadow, activeM);   
+//         mShadow.appendChild(activeM);
 
     
-        //Add color Behind selected row
-        tRow = this.closest('tr');
-        tRow.classList.add('active');
-    }
+//         //Add color Behind selected row
+//         tRow = this.closest('tr');
+//         tRow.classList.add('active');
+//     }
 
-        //Hide Modal
-      var MDismis = document.querySelectorAll('[data-dismiss]');
+//         //Hide Modal
+//       var MDismis = document.querySelectorAll('[data-dismiss]');
 
-      function hideModal(){        
-          var thisModal = this.closest('.gModal');
-          thisModal.classList.add('hide');
-          document.body.removeAttribute('style');
+//       function hideModal(){        
+//           var thisModal = this.closest('.gModal');
+//           thisModal.classList.add('hide');
+//           document.body.removeAttribute('style');
           
-          // remove Shadow
-          // select element to unwrap
-          var mShadow = document.querySelector('.mShadow');
+//           // remove Shadow
+//           // select element to unwrap
+//           var mShadow = document.querySelector('.mShadow');
 
-          // get the element's parent node       
-          var mParent = mShadow.parentNode;
+//           // get the element's parent node       
+//           var mParent = mShadow.parentNode;
 
-          // move all children out of the element
-          while (mShadow.firstChild) mParent.insertBefore(mShadow.firstChild, mShadow);
-          tRow.classList.remove('active');
+//           // move all children out of the element
+//           while (mShadow.firstChild) mParent.insertBefore(mShadow.firstChild, mShadow);
+//           tRow.classList.remove('active');
 
-          // remove the empty element
-          mParent.removeChild(mShadow);
-      }
+//           // remove the empty element
+//           mParent.removeChild(mShadow);
+//       }
 
-      for (j = 0; j < MDismis.length; j++ ){
-        MDismis[j].addEventListener("click", hideModal); 
-      }
+//       for (j = 0; j < MDismis.length; j++ ){
+//         MDismis[j].addEventListener("click", hideModal); 
+//       }
 
 
-    function deleteRecord(){    
-        tRow.parentNode.removeChild(tRow);
-        //hideModal();
-    }
+//     function deleteRecord(){    
+//         tRow.parentNode.removeChild(tRow);
+//         //hideModal();
+//     }
 
-    for(i = 0; i < openModalBtn.length; i++ ){
-        openModalBtn[i].addEventListener("click", openModal); 
-        //deleteBtn[i].onclick = openModal;
-    }
+//     for(i = 0; i < openModalBtn.length; i++ ){
+//         openModalBtn[i].addEventListener("click", openModal); 
+//         //deleteBtn[i].onclick = openModal;
+//     }
 
-    for(k = 0; k < deleteRow.length; k++ ){
-        deleteRow[k].addEventListener("click", deleteRecord); 
-        //deleteBtn[i].onclick = openModal;
-    }
+//     for(k = 0; k < deleteRow.length; k++ ){
+//         deleteRow[k].addEventListener("click", deleteRecord); 
+//         //deleteBtn[i].onclick = openModal;
+//     }
 
-    for(l = 0; l < deleteRow.length; l++ ){
-        deleteRow[l].addEventListener("click", hideModal); 
-        //deleteBtn[i].onclick = openModal;
-    }    
+//     for(l = 0; l < deleteRow.length; l++ ){
+//         deleteRow[l].addEventListener("click", hideModal); 
+//         //deleteBtn[i].onclick = openModal;
+//     }    
 
-}());
+// }());
