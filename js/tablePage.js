84e571676103bb3175d68899050ba43b0aea2295
Amit Parjapati2 <amitp2@chetu.com>
2019-01-16 22:32:02 +0530

    Working on to get this element in current function

--- js/tablePage.js
+++ js/tablePage.js
@@ -1,3 +1,255 @@
+//Open add Record Modal
+(function(){
+    var i, j, k, l;
+    var addRecordBtn = document.querySelectorAll('.addRecord');
+    var activeM = document.getElementById('addRecord');
+    //Open Modal 
+    function addRecord(e){
+        e.preventDefault();
+        //Show Modal
+        activeM.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+
+        //Add dark shadow behind modal
+        var mShadow = document.createElement('div');
+        mShadow.setAttribute('class','mShadow');    
+        activeM.parentNode.insertBefore(mShadow, activeM);   
+        mShadow.appendChild(activeM);    
+    }
+
+    for(i = 0; i < addRecordBtn.length; i++ ){
+        addRecordBtn[i].addEventListener("click", addRecord); 
+        //deleteBtn[i].onclick = openModal;
+    }
+      
+})();
+
+
+var tRow = 'null';
+function deleteModal(self){
+  debugger;
+  //var thisClass = document.
+  //Add color Behind selected row
+  //var data = "$("+self+")";
+  //data = data.replace(/"/,"");
+  tRow = self.closest('.headRow');
+  tRow.classList.add('active');
+ //e.preventDefault();
+  var activeM = document.getElementById('deleteRowModal');
+
+  //Show Modal
+  activeM.classList.remove('hide');
+  document.body.style.overflow = 'hidden';
+
+  // //Add dark shadow behind modal
+  var mShadow = document.createElement('div');
+  mShadow.setAttribute('class','mShadow');    
+  activeM.parentNode.insertBefore(mShadow, activeM);   
+  mShadow.appendChild(activeM);
+
+  
+   
+}
+
+
+
+
+//Close Modal
+$("[data-dismiss='modal']").click(function(){
+  var thisModal = this.closest('.gModal');
+
+
+
+  thisModal.classList.add('hide');
+  document.body.removeAttribute('style');
+
+  // remove Shadow
+  // select element to unwrap
+  var mShadow = document.querySelector('.mShadow');
+
+  // get the element's parent node       
+  var mParent = mShadow.parentNode;
+
+  // move all children out of the element
+  while (mShadow.firstChild) mParent.insertBefore(mShadow.firstChild, mShadow);         
+
+  // remove the empty element
+  mParent.removeChild(mShadow);
+});
+
+
+
+
+
+//Delete a row from table
+// (function(){
+//   debugger;
+//     var i, j, k, l, m;
+//     var deleteModalBtn = document.querySelectorAll('.deleteModal');
+//     //var deleteRow = document.querySelectorAll('.deleteRow')
+//     //var activeM = document.getElementById('deleteRowModal');
+    
+//     //var tRow = 'null';
+
+//     //Open Modal 
+//     function deleteModal(){
+//         // e.preventDefault();
+
+//         // //Show Modal
+//         // activeM.classList.remove('hide');
+//         // document.body.style.overflow = 'hidden';
+
+//         // // //Add dark shadow behind modal
+//         // var mShadow = document.createElement('div');
+//         // mShadow.setAttribute('class','mShadow');    
+//         // activeM.parentNode.insertBefore(mShadow, activeM);   
+//         // mShadow.appendChild(activeM);
+
+    
+//         // //Add color Behind selected row
+//         // tRow = this.closest('tr');
+//         // tRow.classList.add('active');
+//         alert();
+//     }
+
+
+//     // function deleteRecord(){    
+//     //     tRow.parentNode.removeChild(tRow);
+//     //     //hideModal();
+//     // }
+
+//     for(i = 0; i < deleteModalBtn.length; i++ ){
+//       debugger;
+//         deleteModalBtn[i].addEventListener("click", deleteModal); 
+//         //deleteBtn[i].onclick = openModal;
+//     }
+
+//     // for(k = 0; k < deleteRow.length; k++ ){
+//     //     deleteRow[k].addEventListener("click", deleteRecord); 
+//     //     //deleteBtn[i].onclick = openModal;
+//     // }
+
+//     // for(l = 0; l < deleteRow.length; l++ ){
+//     //     deleteRow[l].addEventListener("click", hideModal); 
+//     //     //deleteBtn[i].onclick = openModal;
+//     // }    
+
+// }());
+
+
+
+ 
+$(function(){
+  var $addTodoForm = $('#addTodo');
+  var $tbody = $('tbody');
+
+  var url = 'http://localhost:3000/todos';
+
+  //Template
+  var source = $('#listItemTemplate').html();
+  var template = Handlebars.compile(source)
+
+  //Add TodoList
+  $addTodoForm.on('submit', function(e){
+    e.preventDefault();
+    //alert('test');
+    var newTodo = $addTodoForm.find('input').val();
+
+    
+    $addTodoForm.find('input').val('');
+
+    $.ajax({
+      url:url,
+      method:'POST',
+      data: {
+        text: newTodo
+      }
+    }).done(function(newTodo){
+      var listItem = template({
+            text: newTodo.text,
+            id:newTodo.id
+          });
+
+      $tbody.append(listItem)
+    }).fail(function(){
+      //Err
+    });
+
+    var thisModal = this.closest('.gModal');
+
+    thisModal.classList.add('hide');
+    document.body.removeAttribute('style');
+
+    // remove Shadow
+    // select element to unwrap
+    var mShadow = document.querySelector('.mShadow');
+
+    // get the element's parent node       
+    var mParent = mShadow.parentNode;
+
+    // move all children out of the element
+    while (mShadow.firstChild) mParent.insertBefore(mShadow.firstChild, mShadow);         
+
+    // remove the empty element
+    mParent.removeChild(mShadow);
+
+  });
+
+
+  //Read
+  $.ajax({
+    url:url,
+    method:'GET'
+  }).done(function(data){
+    data.forEach(function(dataitem){
+      var listItem = template({
+        text: dataitem.text,
+        id: dataitem.id
+      });
+
+      $tbody.append(listItem)
+    })
+
+  }).fail(function(){
+    //Error
+  });
+
+
+  //Delete
+  // $tbody.on('click', '.deleteRow', function(e){
+  //   //console.log(e.target)
+  //   var listItem = $(e.target).closest('li');
+  //   var id = listItem.attr('id')
+  //   listItem.remove();
+    
+  //   console.log('id', id)
+
+  //   $.ajax({
+  //     url:url + '/' + id,
+  //     method:'DELETE',
+  //   });
+  // });
+
+  //var editSource = $('#editTemplate').html();
+  //var editTemplate = Handlebars.compile(editSource);
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // function formValidate(){
 //     var name = document.MyForm.name.value;
@@ -57,178 +309,3 @@
 
 //     return( true ); 
 // }
-
-
-//Open add Record Modal
-(function(){
-    var i, j, k, l;
-    var openModalBtn = document.querySelectorAll('.addRecord');
-    var activeM = document.getElementById('addRecord');
-
-    //Open Modal 
-    function openModal(e){
-        e.preventDefault();
-
-        //Show Modal
-        activeM.classList.remove('hide');
-        document.body.style.overflow = 'hidden';
-
-        // //Add dark shadow behind modal
-        var mShadow = document.createElement('div');
-        mShadow.setAttribute('class','mShadow');    
-        activeM.parentNode.insertBefore(mShadow, activeM);   
-        mShadow.appendChild(activeM);
-    
-    }
-
-    for(i = 0; i < openModalBtn.length; i++ ){
-        openModalBtn[i].addEventListener("click", openModal); 
-        //deleteBtn[i].onclick = openModal;
-    }
-
-      var MDismis = document.querySelectorAll('[data-dismiss]');
-
-      function hideModal(){        
-          var thisModal = this.closest('.gModal');
-          thisModal.classList.add('hide');
-          document.body.removeAttribute('style');
-          
-          // remove Shadow
-          // select element to unwrap
-          var mShadow = document.querySelector('.mShadow');
-
-          // get the element's parent node       
-          var mParent = mShadow.parentNode;
-
-          // move all children out of the element
-          while (mShadow.firstChild) mParent.insertBefore(mShadow.firstChild, mShadow);         
-
-          // remove the empty element
-          mParent.removeChild(mShadow);
-      }
-
-      for (j = 0; j < MDismis.length; j++ ){
-        MDismis[j].addEventListener("click", hideModal); 
-      }
-})();
-
-$(function(){
-  var $addTodoForm = $('#addTodo');
-  var $tbody = $('tbody');
-
-  var url = 'http://localhost:3000/todos';
-
-  //Template
-  var source = $('#listItemTemplate').html();
-  var template = Handlebars.compile(source)
-
-  //Add TodoList
-  $addTodoForm.on('submit', function(e){
-    e.preventDefault();
-    //alert('test');
-    var newTodo = $addTodoForm.find('input').val();
-
-    
-    $addTodoForm.find('input').val('');
-
-    $.ajax({
-      url:url,
-      method:'POST',
-      data: {
-        text: newTodo
-      }
-    }).done(function(newTodo){
-      var listItem = template({
-            text: newTodo.text,
-            id:newTodo.id
-          });
-
-      $tbody.append(listItem)
-    }).fail(function(){
-      //Err
-    })
-  });
-})
-
-
-
-
-//Delete a row from table
-// (function(){
-//     var i, j, k, l, m;
-//     var openModalBtn = document.querySelectorAll('.openModal');
-//     var deleteRow = document.querySelectorAll('.deleteRow')
-//     var activeM = document.getElementById('deleteRowModal');
-
-    
-//     var tRow = 'null';
-
-//     //Open Modal 
-//     function openModal(e){
-//         e.preventDefault();
-
-//         //Show Modal
-//         activeM.classList.remove('hide');
-//         document.body.style.overflow = 'hidden';
-
-//         // //Add dark shadow behind modal
-//         var mShadow = document.createElement('div');
-//         mShadow.setAttribute('class','mShadow');    
-//         activeM.parentNode.insertBefore(mShadow, activeM);   
-//         mShadow.appendChild(activeM);
-
-    
-//         //Add color Behind selected row
-//         tRow = this.closest('tr');
-//         tRow.classList.add('active');
-//     }
-
-//         //Hide Modal
-//       var MDismis = document.querySelectorAll('[data-dismiss]');
-
-//       function hideModal(){        
-//           var thisModal = this.closest('.gModal');
-//           thisModal.classList.add('hide');
-//           document.body.removeAttribute('style');
-          
-//           // remove Shadow
-//           // select element to unwrap
-//           var mShadow = document.querySelector('.mShadow');
-
-//           // get the element's parent node       
-//           var mParent = mShadow.parentNode;
-
-//           // move all children out of the element
-//           while (mShadow.firstChild) mParent.insertBefore(mShadow.firstChild, mShadow);
-//           tRow.classList.remove('active');
-
-//           // remove the empty element
-//           mParent.removeChild(mShadow);
-//       }
-
-//       for (j = 0; j < MDismis.length; j++ ){
-//         MDismis[j].addEventListener("click", hideModal); 
-//       }
-
-
-//     function deleteRecord(){    
-//         tRow.parentNode.removeChild(tRow);
-//         //hideModal();
-//     }
-
-//     for(i = 0; i < openModalBtn.length; i++ ){
-//         openModalBtn[i].addEventListener("click", openModal); 
-//         //deleteBtn[i].onclick = openModal;
-//     }
-
-//     for(k = 0; k < deleteRow.length; k++ ){
-//         deleteRow[k].addEventListener("click", deleteRecord); 
-//         //deleteBtn[i].onclick = openModal;
-//     }
-
-//     for(l = 0; l < deleteRow.length; l++ ){
-//         deleteRow[l].addEventListener("click", hideModal); 
-//         //deleteBtn[i].onclick = openModal;
-//     }    
-
-// }());
