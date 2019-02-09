2011ec99571eea40699a32011eb0f05691ffba30
Amit Parjapati2 <amitp2@chetu.com>
2019-02-09 19:25:05 +0530

    Working on JSON

--- js/tablePage4.js
+++ js/tablePage4.js
@@ -1,124 +1,137 @@
 
 $(function(){
 
-  var $addDataForm = $('[name=fillRecord]');
-  var $tbody = $('tbody');  
-  var url = "./js/db/user4.json";
-  //Template
-  //var source = $('#tableRow').html();
-  //var template = Handlebars.compile(source)
+    //Change select dropdown color
+    $('select').on('change', function() {
+        if ($(this).val() == "-1") {
+            return $(this).css('color', '#8e8e8e');
+        } else {
+            return $(this).css('color', '#000');
+        }
+    });
 
-  //Add TodoList
-  $addDataForm.on('submit', function(e){
-    e.preventDefault();
-    //alert('test');       
-    
-    var newFName = $addDataForm.find('input[name="fname"]');
-    var newLName = $addDataForm.find('input[name="lname"]');
-    var newAge = $addDataForm.find('input[name="age"]');
-    var newDob = $addDataForm.find('input[name="dob"]');
-    var newOcc = $addDataForm.find('select[name="occ"]');
+    var $addDataForm = $('[name=fillRecord]');
+    var $tbody = $('tbody');  
+    var url = "./js/db/user4.json";  
 
-    if( newFName.val() == "" ){
-        alert( "Please provide First Name!" );
-        newFName.focus();
-        return false;
-    }
+    //Add TodoList
+    $addDataForm.on('submit', function(e){
+        e.preventDefault();
+        //alert('test');       
 
-    else if( newLName.val() == "" ){
-        alert( "Please provide Last Name!" );
-        newLName.focus();
-        return false;
-    }
+        var newFName = $addDataForm.find('input[name="fname"]');
+        var newLName = $addDataForm.find('input[name="lname"]');        
+        var newDob = $addDataForm.find('input[name="dob"]');
+        var newExp = $addDataForm.find('input[name="experience"]');
+        var newOccupation = $addDataForm.find('select[name="occ"]');
 
-    else if( newAge.val() == "" ){
-        alert( "Please provide Age!" );
-        newAge.focus();
-        return false;
-    }
+        if( newFName.val() == "" ){
+            alert( "Please provide First Name!" );
+            newFName.focus();
+            return false;
+        }
 
-    else if( isNaN( newAge.val() ) ){
-        alert( "Enter only numberic digits in Age" );
-        newAge.focus();
-        return false;
-    }
+        if( newLName.val() == "" ){
+            alert( "Please provide Last Name!" );
+            newLName.focus();
+            return false;
+        }
+        
+        if( newDob.val() == "" ){
+            alert( "Please provide Date of Birth!" );
+            newDob.focus();
+            return false;
+        }
 
-    else if( newAge.val().length > 2 ){
-        alert( "Enter Valid Age!" );
-        newAge.focus();
-        return false;
-    }
+        if( newExp.val() == "" ){
+            alert( "Please provide Experience!" );
+            newExp.focus();
+            return false;
+        }
 
-    else if( newDob.val() == "" ){
-        alert( "Please provide Date of Birth!" );
-        newDob.focus();
-        return false;
-    }
+        if( isNaN( newExp.val() ) ){
+            alert( "Enter only numberic digits in Experience" );
+            newExp.focus();
+            return false;
+        }
 
-    else if( newOcc.val() == "-1" ){
-    	newOcc.focus();
-        alert( "Please provide Occupation!" );
-        return false;
-    }
+        if( newExp.val().length > 2 ){
+            alert( "Enter Valid Experience!" );
+            newExp.focus();
+            return false;
+        }
 
-    else{
-    	return true 
-    }
+        if( newOccupation.val() == "-1" ){        
+            newOccupation.focus();
+            alert( "Please provide Occupation!" );
+            return false;
+        }
 
-    $addDataForm.find('input').val('');
-    $addDataForm.find('select').val('-1');
+        //return( true ); 
 
-    $.ajax({
-      url:url,
-      method:'POST',
-      data: {
-        firstName: firstName,
-        lastName: lastName,
-        age: age,
-        dob: dob,
-        occupatoin: occ
-      }
-    }).done(function(newId){
-      var listItem = template({
-            firstName: newId.firstName,
-            lastName: newId.lastName,
-            age: newId.age,
-            dob: newId.dob,
-            occ: newId.occ,
-            id:newId.id
-          });
+        $addDataForm.find('input').val('');
+        $addDataForm.find('select').val('-1');
 
-      $tbody.append(listItem)
-    }).fail(function(){
-      //Err
-    }); 
+        // $.ajax({        
+        //   url:url,
+        //   method:'POST',     
+        //   data: {
+        //     firstName: newFName.val(),
+        //     lastName: newLName.val(),
+        //     age: newAge.val(),
+        //     dob: newDob.val(),
+        //     occupatoin: newOccupation.val()
+        //   }
+        // }).done(function(newId){
+        //    var tr = $('<tr/>');
+        //    tr.append("<td>" + newId.firstName + "</td>");
+        //    tr.append("<td>" + newId.lastName + "</td>");
+        //    tr.append("<td>" + newId.age + "</td>");
+        //    tr.append("<td>" + newId.dob + "</td>");
+        //    tr.append("<td>" + newId.occupatoin + "</td>");
+        //    tr.append("<td>" + newId.id + "</td>");         
+        // });
 
+        //   $tbody.append(tr)
+        // }).fail(function(){
+        //   //Err
+        // });
+
+        var tr = $('<tr/>');
+        tr.append("<td>" + newFName.val() + "</td>");
+        tr.append("<td>" + newLName.val() + "</td>");       
+        tr.append("<td>" + newDob.val() + "</td>");
+         tr.append("<td>" + newExp.val() + "</td>");
+        tr.append("<td>" + newOccupation.val() + "</td>");
+        $tbody.append(tr);    
+    });
+    //end form submit method
+
+
+    //Read JSON Data
+    $.getJSON(url, function (data) {
+        var tr;
+        for (var i = 0; i < data.users.length; i++) {
+            tr = $('<tr/>');
+            tr.append("<td>" + data.users[i].id + "</td>");
+            tr.append("<td>" + data.users[i].firstname + "</td>");
+            tr.append("<td>" + data.users[i].lastname + "</td>");            
+            tr.append("<td>" + data.users[i].dob + "</td>");
+            tr.append("<td>" + data.users[i].experience + "</td>");
+            tr.append("<td>" + data.users[i].occupatoin + "</td>");
+            tr.append("<td>" + '<button class="btn btn-warning deleteRow" onclick=deleteRowModal(event);>' + 
+            '<i class="fa fa-trash"></i>' +
+            '</button>' +
+            '<button class="btn btn-warning editRow">' + 
+            '<i class="fa fa-pencil"></i>' + 
+            '</button>' + "</td>");
+            $tbody.append(tr);
+            console.log(data);
+        }
+    });
+    //End getJson
 });
-
-
-
-	//Read JSON Data
-	$.getJSON(url, function (data) {
-	    var tr;
-	    for (var i = 0; i < data.users.length; i++) {
-	        tr = $('<tr/>');
-	        tr.append("<td>" + data.users[i].id + "</td>");
-	        tr.append("<td>" + data.users[i].firstname + "</td>");
-	        tr.append("<td>" + data.users[i].lastname + "</td>");
-	        tr.append("<td>" + data.users[i].age + "</td>");
-	        tr.append("<td>" + data.users[i].dob + "</td>");
-	        tr.append("<td>" + data.users[i].occupatoin + "</td>");
-	        tr.append("<td>" + '<button class="btn btn-warning deleteRow" onclick=deleteRowModal(event);>' + 
-					        	'<i class="fa fa-trash"></i>' +
-					        	'</button>' +
-            					'<button class="btn btn-warning editRow">' + 
-            					'<i class="fa fa-pencil"></i>' + 
-            					'</button>' + "</td>");
-	        $tbody.append(tr);
-	        console.log(data);
-	    }
-	});
-});
+//End function
 
 
 
--- js/tablePage5.js
+++ js/tablePage5.js
@@ -1,6 +1,6 @@
-var url = 'http://localhost:3000/users';
+var url = './js/db/user4.json';
 
-function loadDoc() {
+(function() {
 	var i, xhttp, tbody, myObj, txt='',
    	xhttp = new XMLHttpRequest();
 
@@ -9,15 +9,16 @@ function loadDoc() {
 	if (this.readyState == 4 && this.status == 200) {
 		
 	  	myObj = JSON.parse(this.responseText);
+	  	myObj.dob = new Date(myObj.dob);
 
-	  	for (i=0; i < myObj.length; i++) {
-	  		txt += "<tr id=row_"+ myObj[i].id + ">" +
-	  			"<td>" + myObj[i].id + "</td>" + 
-	  			"<td>" + myObj[i].firstname + "</td>" +
-	  			"<td>" + myObj[i].lastname + "</td>" +
-	  			"<td>" + myObj[i].age + "</td>" +
-	  			"<td>" + myObj[i].dob + "</td>" +
-	  			"<td>" + myObj[i].occupatoin + "</td>" +
+	  	for (i=0; i < myObj.users.length; i++) {
+	  		txt += "<tr id=row_"+ myObj.users[i].id + ">" +
+	  			"<td>" + myObj.users[i].id + "</td>" + 
+	  			"<td>" + myObj.users[i].firstname + "</td>" +
+	  			"<td>" + myObj.users[i].lastname + "</td>" +	  			
+	  			"<td>" + myObj.users[i].dob + "</td>" +
+	  			"<td>" + myObj.users[i].experience + "</td>" +
+	  			"<td>" + myObj.users[i].occupatoin + "</td>" +
 	  			"<td>" + "<button class='btn btn-warning deleteRow' onclick='deleteRow(event);'><i class='fa fa-trash'></i></button>" +
             "<button class='btn btn-warning editRow'><i class='fa fa-pencil'></i></button>" + "</td>" +
 	  		  "</tr>";
@@ -28,23 +29,33 @@ function loadDoc() {
 	};
 	xhttp.open("GET", url, true);
 	xhttp.send();
-}
-
-loadDoc();
-
-(function(){
-	var table = document.getElementById( "myTable" );	
-	var tableArr = [];
-	var fName;
-	for ( var i = 1; i < table.rows.length; i++ ) {
-	    tableArr.push({
-	        fName: table.rows[i].innerHTML      
-	    });
-	}
-
-	console.log(tableArr)
 })();
 
+
+function myFunction() {
+  // Declare variables 
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("search");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++ ) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    } 
+  }
+}
+
+
+//Delete table row
 function deleteRow(event){
 	var x = event.target;
 	var tRow = x.closest('tr')
