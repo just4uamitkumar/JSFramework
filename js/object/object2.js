afab5fa085e10cf459482e04a6700861a5ad71fe
Amit Parjapati2 <amitp2@chetu.com>
2019-03-08 23:51:46 +0530

    working on to update oops

--- js/object/object2.js
+++ js/object/object2.js
@@ -1,10 +1,14 @@
 
 var employee= {ID : 2103, Name :"Darell Laurid", Designation:"Software Engineer"}
 
-document.getElementById('demo1').innerHTML = employee.Name;
+document.getElementById('demo1').innerHTML = employee.Designation +', '+ employee.Name;
 
 
 
+
+var student = { RollNo : 5, Name :"David", Subject:"English"}
+
+document.getElementById('demo2').innerHTML = student['Name'] +', '+ student['Subject'];
 
 
 
@@ -13,3 +17,7 @@ document.getElementById('demo1').innerHTML = employee.Name;
 document.getElementById('var1A').innerHTML = employee.ID 
 document.getElementById('var1B').innerHTML = employee.Name 
 document.getElementById('var1C').innerHTML = employee.Designation 
+
+document.getElementById('var2A').innerHTML = student.RollNo 
+document.getElementById('var2B').innerHTML = student.Name 
+document.getElementById('var2C').innerHTML = student.Subject 
