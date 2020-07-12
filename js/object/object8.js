4303295dfc899c4a59fd64ec72a5b4bf11d6c0c0
just4uamitkumar <just4uamitkumar@gmail.com>
2020-07-12 19:33:45 +0530

    using js

---
+++ js/object/object8.js
@@ -0,0 +1,68 @@
+function GetData(){
+
+    var jsonData = []
+
+    fetch(`./js/db/user4.json`) //Local Json
+    .then((data) => { return data.json()
+    })
+    .then( (data2) => {
+
+       jsonData = jsonData.concat(data2)
+
+       console.log(jsonData[0].users); //Get Data Here
+      
+    });
+
+    
+   
+   console.log(jsonData[0].users) 
+   //Uncaught TypeError: Cannot read property 'users' of undefined
+  
+};
+
+function GetData2(){
+  const jsonData = [];
+
+  const myarr = [
+        {"id":1,"firstname":"Smith","lastname":"Clark", 
+       "dob":"1988-06-14","experience":"2","occupatoin":"Web Designer"},
+
+      {"id":2,"firstname":"Baker","lastname":"Jackson", 
+       "dob":"1987-12-20","experience":"2.5","occupatoin":"Web Developer"},
+
+      {"id":3,"firstname":"Williams","lastname":"Grande", 
+      "dob":"1992-05-24","experience":"1","occupatoin":"Data Entry Operator"},
+
+      {"id":4,"firstname":"Jones","lastname":"Kenedy", "dob":"1990-07-28",
+      "experience":"5","occupatoin":"Quality Assurance"},
+
+      {"id":5,"firstname":"Brown","lastname":"Poe",  "dob":"1986-12-14",
+      "experience":"4","occupatoin":"Graphic Designer"},
+
+      {"id":6,"firstname":"Moore","lastname":"Winfrey",
+       "dob":"1984-09-16","experience":"3.5","occupatoin":"Web Designer"},
+
+      {"id":7,"firstname":"Allen","lastname":"Rancic","dob":"1991-01-10",
+      "experience":"2.6","occupatoin":"Web Developer"}
+  ]
+
+
+  //jsonData.push(myarr)
+  //console.log(jsonData)
+
+  const b = myarr.find(({id}) => id === 7)
+  
+  console.log(b)
+
+  const inventory = [
+    {name: 'apples', quantity: 2},
+    {name: 'bananas', quantity: 0},
+    {name: 'cherries', quantity: 5}
+  ];
+
+  const result = inventory.find( ({ name }) => name === 'cherries' );
+  console.log(result)
+
+  
+}
+
