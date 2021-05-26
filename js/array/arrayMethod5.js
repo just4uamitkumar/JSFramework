9db107e323f910ac25cc28b515a640532b9ddde4
just4uamitkumar <just4uamitkuamr@gmail.com>
2021-05-26 18:11:02 +0530

    transform aarray

--- js/array/arrayMethod5.js
+++ js/array/arrayMethod5.js
@@ -1,30 +1,14 @@
 
 function transFormString(myString){ 
-  let a = myString.split('')
-  for(let i = 0 ; i < a.length; i++){ 
-  setTimeout(function(){
-    if(a[i] == '_'){
-        if(a[i])
-        a[i+1] = a[i+1].toUpperCase();
-        a[i] = ''
-        a.toString()
-        a.join('')
-    }
-  }, 100 )    
-    
+  let a = myString.split('_')
+  for(let i = 1 ; i < a.length; i++){ 
+    a[i] = a[i].charAt(0).toUpperCase() + a[i].slice(1);
   }
-}
 
-
-function humanize(str) {
-  var i, frags = str.split('_');
-  for (i=1; i<frags.length; i++) {
-    frags[i] = frags[i].charAt(0).toUpperCase() + frags[i].slice(1);
-  }  
-  return frags.join('');
+   return a.join('');
 }
 
 
 let x = document.getElementById('demo1').innerHTML;
 
-document.getElementById('demo2').innerHTML = humanize(x)
+document.getElementById('demo2').innerHTML = transFormString(x)
